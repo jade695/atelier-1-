976da218fc3a6b90598c6482179da5b5ff23bf6e
jade695 <jade.alvarez@esme.fr>
2025-09-12 08:59:37 +0000

    atelier 1

--- program.c
+++ program.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Bonjour tout le monde!\n");
+    printf(" |-0  |\n");
+    printf(" |    |\n");
+    printf(" |-20 |\n");
+    printf(" |   \n" );
+    printf(" |-40 \n");
+    printf("     \n");
+    printf(" |-60 \n");
+    printf("     \n");
+    printf(" |-80 \n");
+    printf("     \n");
+    printf(" |-100\n");
+    printf("     \n");
+    printf(" |-120\n");
+    printf("     \n");
+    printf(" |-140\n");
+
+        
+         
     return 0;
 }
