965bdb58dd08522991e7804f1d353e3527da8967
jade695 <jade.alvarez@esme.fr>
2025-09-12 17:19:03 +0000

    atelier 1

--- program.c
+++ program.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf(" |-0  |\n");
-    printf(" |    |\n");
-    printf(" |-20 |\n");
-    printf(" |   \n" );
-    printf(" |-40 \n");
-    printf("     \n");
-    printf(" |-60 \n");
-    printf("     \n");
-    printf(" |-80 \n");
-    printf("     \n");
-    printf(" |-100\n");
-    printf("     \n");
-    printf(" |-120\n");
-    printf("     \n");
-    printf(" |-140\n");
-
-        
-         
-    return 0;
+    printf("  _____ \n");
+    printf(" |-0   |\n");
+    printf(" |     |\n");
+    printf(" |-20  |\n");
+    printf(" |     |\n");
+    printf(" |-40  |\n");
+    printf(" |     |\n");
+    printf(" |-60  |\n");
+    printf(" |     |\n");
+    printf(" |-80  |\n");
+    printf(" |     |\n");
+    printf(" |-100 |\n");
+    printf(" |     |\n");
+    printf(" |-120 |\n");
+    printf(" |     |\n");
+    printf(" |-140 |\n");
+    printf(" |_____|\n");
+return 0;
 }
