252ec08b81c0b99eb706c3434b35bcc734ec3e8d
jade695 <jade.alvarez@esme.fr>
2025-09-16 20:03:36 +0000

    fi

--- program.c
+++ program.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main() {
     printf("  _____ \n");
-    printf(" |-0   |\n");
-    printf(" |     |\n");
-    printf(" |-20  |\n");
-    printf(" |     |\n");
-    printf(" |-40  |\n");
-    printf(" |     |\n");
-    printf(" |-60  |\n");
-    printf(" |     |\n");
-    printf(" |-80  |\n");
-    printf(" |     |\n");
-    printf(" |-100 |\n");
+    printf(" |-140 |\n");
     printf(" |     |\n");
     printf(" |-120 |\n");
     printf(" |     |\n");
-    printf(" |-140 |\n");
+    printf(" |-100 |\n");
+    printf(" |     |\n");
+    printf(" |-80  |\n");
+    printf(" |     |\n");
+    printf(" |-60  |\n");
+    printf(" |     |\n");
+    printf(" |-40  |\n");
+    printf(" |     |\n");
+    printf(" |-20  |\n");
+    printf(" |     |\n");
+    printf(" |-0   |\n");
     printf(" |_____|\n");
-    printf("                                       v                 \n");
+    printf("                                       v                \n");
     printf("                       |---|---|---|---|---|---|---|---|\n");
     printf("                       |   6   7   8   9   10  11  12  |\n");
     
